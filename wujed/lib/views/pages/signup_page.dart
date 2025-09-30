91b24abf096179979d577728c2bd8da62a5a505d
Ghaida45 <ghaydamo0@gmail.com>
2025-09-30 23:11:34 +0300

    age checkbox

--- wujed/lib/views/pages/signup_page.dart
+++ wujed/lib/views/pages/signup_page.dart
@@ -31,229 +31,273 @@ class _SignupPageState extends State<SignupPage> {
   Color signupBtnColor = Colors.grey.shade400;
 
   bool hidePassword = true;
+  bool is18OrOlder = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Join Us!',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26.0),
-              ),
-
-              SizedBox(height: 10.0),
-
-              Text(text, style: TextStyle(fontSize: 16.0, color: textColor)),
-
-              SizedBox(height: 50.0),
-
-              TextField(
-                controller: controllerUsername,
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  labelStyle: TextStyle(fontSize: 16.0),
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                  floatingLabelStyle: TextStyle(
-                    color: Color.fromRGBO(46, 23, 21, 1),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Join Us!',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26.0),
+                ),
+                  
+                SizedBox(height: 10.0),
+                  
+                Text(text, style: TextStyle(fontSize: 16.0, color: textColor)),
+                  
+                SizedBox(height: 50.0),
+                  
+                TextField(
+                  controller: controllerUsername,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(fontSize: 16.0),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    floatingLabelStyle: TextStyle(
                       color: Color.fromRGBO(46, 23, 21, 1),
-                      width: 2.0,
                     ),
-                  ),
-                ),
-                onEditingComplete: () {
-                  setState(() {});
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    takenUsernameWarning,
-                    style: TextStyle(color: Color.fromRGBO(211, 47, 47, 1)),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 10.0),
-
-              TextField(
-                controller: controllerEmail,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(fontSize: 16.0),
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                  floatingLabelStyle: TextStyle(
-                    color: Color.fromRGBO(46, 23, 21, 1),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide(
-                      color: Color.fromRGBO(46, 23, 21, 1),
-                      width: 2.0,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
                     ),
-                  ),
-                ),
-                onEditingComplete: () {
-                  setState(() {});
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    existingEmailWarning,
-                    style: TextStyle(color: Color.fromRGBO(211, 47, 47, 1)),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 10.0),
-
-              TextField(
-                controller: controllerPassword,
-                obscureText: hidePassword,
-                obscuringCharacter: '*',
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(fontSize: 16.0),
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                  floatingLabelStyle: TextStyle(
-                    color: Color.fromRGBO(46, 23, 21, 1),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide(
-                      color: Color.fromRGBO(46, 23, 21, 1),
-                      width: 2.0,
-                    ),
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        hidePassword = !hidePassword;
-                      });
-                    },
-                    icon: Icon(
-                      hidePassword
-                          ? Icons.visibility_off_outlined
-                          : Icons.visibility_outlined,
-                      color: Colors.grey.shade600,
-                    ),
-                  ),
-                ),
-                onEditingComplete: () {
-                  setState(() {});
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    weakPasswordWarning,
-                    style: TextStyle(color: Color.fromRGBO(211, 47, 47, 1)),
-                  ),
-                ],
-              ),
-
-              Image.asset('lib/assets/images/reCAPTCHA.png', width: 400,),
-
-              SizedBox(height: 30.0),
-
-              FilledButton(
-                onPressed: () {
-                  onLoginPressed();
-                },
-                style: FilledButton.styleFrom(
-                  minimumSize: Size(double.infinity, 50),
-                  backgroundColor: signupBtnColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                ),
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                ),
-              ),
-
-              SizedBox(height: 10.0),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Already have an account?',
-                    style: TextStyle(color: Colors.grey.shade600),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return LoginPage();
-                          },
-                        ),
-                        (route) => false,
-                      );
-                    },
-                    child: Text(
-                      'Log In',
-                      style: TextStyle(
-                        color: Color.fromRGBO(0, 111, 255, 1),
-                        decoration: TextDecoration.underline,
-                        decorationThickness: 1,
-                        decorationColor: Color.fromRGBO(0, 111, 255, 1),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(46, 23, 21, 1),
+                        width: 2.0,
                       ),
                     ),
                   ),
-                ],
-              ),
-
-              SizedBox(height: 10.0),
-
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(color: Colors.grey.shade400, thickness: 1),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      'OR',
-                      style: TextStyle(color: Colors.grey.shade600),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      takenUsernameWarning,
+                      style: TextStyle(color: Color.fromRGBO(211, 47, 47, 1)),
+                    ),
+                  ],
+                ),
+                  
+                SizedBox(height: 10.0),
+                  
+                TextField(
+                  controller: controllerEmail,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(fontSize: 16.0),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    floatingLabelStyle: TextStyle(
+                      color: Color.fromRGBO(46, 23, 21, 1),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(46, 23, 21, 1),
+                        width: 2.0,
+                      ),
                     ),
                   ),
-                  Expanded(
-                    child: Divider(color: Colors.grey.shade400, thickness: 1),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      existingEmailWarning,
+                      style: TextStyle(color: Color.fromRGBO(211, 47, 47, 1)),
+                    ),
+                  ],
+                ),
+                  
+                SizedBox(height: 10.0),
+                  
+                TextField(
+                  controller: controllerPassword,
+                  obscureText: hidePassword,
+                  obscuringCharacter: '*',
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(fontSize: 16.0),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    floatingLabelStyle: TextStyle(
+                      color: Color.fromRGBO(46, 23, 21, 1),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(46, 23, 21, 1),
+                        width: 2.0,
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          hidePassword = !hidePassword;
+                        });
+                      },
+                      icon: Icon(
+                        hidePassword
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
                   ),
-                ],
-              ),
-
-              SizedBox(height: 20.0),
-
-              GsiMaterialButton(onPressed: () {}, text: 'Sign up with Google'),
-            ],
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      weakPasswordWarning,
+                      style: TextStyle(color: Color.fromRGBO(211, 47, 47, 1)),
+                    ),
+                  ],
+                ),
+                  
+                Image.asset('lib/assets/images/reCAPTCHA.png', width: 400),
+                  
+                SizedBox(height: 5.0),
+                  
+                Row(
+                  children: [
+                    Theme(
+                      data: Theme.of(context).copyWith(
+                        checkboxTheme: CheckboxThemeData(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          side: const BorderSide(color: Colors.grey, width: 2),
+                          fillColor: WidgetStateProperty.resolveWith<Color>((
+                            states,
+                          ) {
+                            if (states.contains(WidgetState.selected)) {
+                              return const Color.fromRGBO(46, 23, 21, 1);
+                            }
+                            return Colors.white;
+                          }),
+                          checkColor: WidgetStateProperty.all<Color>(
+                            Colors.white,
+                          ),
+                        ),
+                      ),
+                      child: Checkbox(
+                        value: is18OrOlder,
+                        onChanged: (value) {
+                          setState(() {
+                            is18OrOlder = value ?? false;
+                            updateButtonColor();
+                          });
+                        },
+                      ),
+                    ),
+                    const Expanded(
+                      child: Text(
+                        "I am 18 years old or older",
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                    ),
+                  ],
+                ),
+                  
+                FilledButton(
+                  onPressed: () {
+                    onLoginPressed();
+                  },
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 50),
+                    backgroundColor: signupBtnColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                  
+                SizedBox(height: 10.0),
+                  
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return LoginPage();
+                            },
+                          ),
+                          (route) => false,
+                        );
+                      },
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Color.fromRGBO(0, 111, 255, 1),
+                          decoration: TextDecoration.underline,
+                          decorationThickness: 1,
+                          decorationColor: Color.fromRGBO(0, 111, 255, 1),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                  
+                SizedBox(height: 10.0),
+                  
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(color: Colors.grey.shade400, thickness: 1),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        'OR',
+                        style: TextStyle(color: Colors.grey.shade600),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(color: Colors.grey.shade400, thickness: 1),
+                    ),
+                  ],
+                ),
+                  
+                SizedBox(height: 20.0),
+                  
+                GsiMaterialButton(onPressed: () {}, text: 'Sign up with Google'),
+              ],
+            ),
           ),
         ),
       ),
@@ -264,7 +308,8 @@ class _SignupPageState extends State<SignupPage> {
     final filled =
         controllerUsername.text.trim().isNotEmpty &&
         controllerEmail.text.trim().isNotEmpty &&
-        controllerPassword.text.isNotEmpty;
+        controllerPassword.text.isNotEmpty &&
+        is18OrOlder;
 
     setState(() {
       signupBtnColor = filled
