5dcb49845ecb14e10fc79406af1371eb43a9b390
v <halooh2004@gmail.com>
2025-10-24 23:17:14 +0300

    Update build.gradle.kts

--- wujed/android/app/build.gradle.kts
+++ wujed/android/app/build.gradle.kts
@@ -7,17 +7,17 @@ plugins {
 }
 
 android {
-    namespace = "com.example.wujed"
+    namespace = "com.gp.wujed"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
